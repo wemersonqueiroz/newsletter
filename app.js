8719b8ded0b64698ff97092001c9e932c892bc53
Wemerson <wemersonqueiroz@yahoo.com>
2022-04-22 06:10:23 +0100

    [Feat]Configured Initial Local Server Settings.

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const Test = Test => console.log("Test OK!")
+
+app.listen(3000, function () {
+  Test()
+})
